61ac2d74a4e3c37e6f2cd8454c19bf43f2355f06
sumukshashidhar <sumukuuu@gmail.com>
2020-04-23 10:14:03 +0530

    TileGame update

---
+++ FRQs/TileGame.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+
+public class TileGame{
+
+	private ArrayList<NumberTile> board;
+
+	public TileGame() {
+		
+
+	
+
+	}
+
+
+	private int getIndexForFit (NumberTile tile) {
+
+		
+
+
+	}
+
+
+	public boolean insertTile(NumberTile tile) {
+	
+		
+
+	}
+
+
+
+
+
+}
+
+
+
+public class NumberTile {
+
+
+	public void rotate() {
+	
+		//rotates the tile 90 degrees
+
+	
+	}
+
+
+	public int getLeft() {
+
+		// returns the value at the left edge of the tile
+
+
+	}	
+
+
+	public int getRight() {
+
+		//returns value at the right edge
+
+	}
+
+
+	public NumberTile(int[] tilearr){
+		
+		// making the numbertile
+
+		
+
+	}
+
+
+
+
+}
