ebc41ffbd7f9f55fbadf843a3c419c0e76c1ac3a
sumukshashidhar <sumukuuu@gmail.com>
2020-04-25 21:57:54 +0530

    finished frq1

--- FRQs/2017FRQ/q1.java
+++ FRQs/2017FRQ/q1.java
@@ -1,45 +1,68 @@
-import java.util.ArrayList;
-class q1 {
+import java.util.*;
+public class q1 {
 
 
 	public static void main(String[] args) {
 		System.out.println(new Digits(7).isStrictlyIncreasing());
+		System.out.println();
+		System.out.println();
+		System.out.println(new Digits(1356).isStrictlyIncreasing());
+		System.out.println();
+		System.out.println();
+		System.out.println(new Digits(1336).isStrictlyIncreasing());
+		System.out.println();
+		System.out.println();
+		System.out.println(new Digits(1536).isStrictlyIncreasing());
+		System.out.println();
+		System.out.println();
+		System.out.println(new Digits(65310).isStrictlyIncreasing());
 	}
 
-	public class Digits {
-
-		private ArrayList<Integer> digitList = new ArrayList<Integer>();
+}
 
 
-		public Digits(int num) {
-			if(num == 0) {
-				this.digitList.add(num);
-			}
-			while(num > 0) {
-				int single = num%10;
-				num = (int)num/10; 
-				this.digitList.add(single)
-			}
+public class Digits {
+
+	private ArrayList<Integer> digitList = new ArrayList<Integer>();
+
+
+	public Digits(int num) {
+		if(num == 0) {
+			this.digitList.add(num);
 		}
+		while(num > 0) {
+			int single = num%10;
+			num = (int)num/10; 
+			this.digitList.add(single);
+		}
+		Collections.reverse(this.digitList);
+	}
 
-		public boolean isStrictlyIncreasing() {
-			boolean hasIncreased = true;
-			int prevDigit = 0; 
-			for(int digit: digitList) {
-				if(digit > prevDigit) {
-					hasIncreased = true;
-					prevDigit = digit; 
-				}
-				else {
-					hasIncreased = false;
-				}
-			}
-			if(hasIncreased == true) {
-				return true;
+	public boolean isStrictlyIncreasing() {
+		boolean hasIncreased = true;
+		int prevDigit = 0; 
+		for(int digit: this.digitList) {
+			// System.out.print(digit);
+			if(digit > prevDigit) {
+				// System.out.println(digit);
+				hasIncreased = true;
+				prevDigit = digit; 
 			}
 			else {
-				return false;
+				hasIncreased = false;
+				break;
 			}
 		}
+		if(hasIncreased == true) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	public void main(String[] args) {
+		boolean isTrue = new Digits(7).isStrictlyIncreasing();
+		System.out.println(isTrue);
 	}
 }
